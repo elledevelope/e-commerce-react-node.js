6dd44ff43676bb651c9f2e7e87fd3907b41239ea
Elmira PETIT <elledevelope@gmail.com>
2024-05-30 15:38:50 +0200

    Created Navbar structure and style

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
+const Navbar = () => {
+    return (
+        <div className="navbar">
 
-export const Navbar = () => {
-  return (
-    <div>navbar</div>
-  )
+            <div className="nav-logo">
+                <img src={logo} alt="logo" />
+                <p>SHOPPER</p>
+            </div>
+
+            <ul className="nav-menu">
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
+
+            <div className="nav-login-cart">
+                <img src={cart_icon} alt="cart icon" />
+                <button>Login</button>
+            </div>
+
+        </div>
+    )
 }
+
+export default Navbar
