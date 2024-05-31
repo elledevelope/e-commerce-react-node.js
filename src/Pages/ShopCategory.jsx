5e84b48d2730338e0c76e30103c05a25be58f005
Elmira PETIT <elledevelope@gmail.com>
2024-05-31 16:11:41 +0200

    Created ShopCategory

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
 
-export const ShopCategory = () => {
+export const ShopCategory = (props) => {
+
+  const {all_product} = useContext(ShopContext);
+
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      <img src={props.banner} alt={props.banner} />
+    </div>
   )
 }
